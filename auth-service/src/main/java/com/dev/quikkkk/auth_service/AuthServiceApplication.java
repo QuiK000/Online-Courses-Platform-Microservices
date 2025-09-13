8acfb2ab6a78a85ea4d118d46300a6c0a30ab920
quikkkk <kexitttttt@gmail.com>
2025-09-13 17:54:00 +0300

    add CommandLineRunner for initial role seeding

--- auth-service/src/main/java/com/dev/quikkkk/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/dev/quikkkk/auth_service/AuthServiceApplication.java
@@ -1,13 +1,45 @@
 package com.dev.quikkkk.auth_service;
 
+import com.dev.quikkkk.auth_service.entity.Role;
+import com.dev.quikkkk.auth_service.repository.IRoleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Optional;
 
 @SpringBootApplication
+@Slf4j
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
+    @Bean
+    public CommandLineRunner commandLineRunner(IRoleRepository repository) {
+        return args -> {
+            createRoleIfNotExists(repository, "ROLE_STUDENT");
+            createRoleIfNotExists(repository, "ROLE_TEACHER");
+            createRoleIfNotExists(repository, "ROLE_ADMIN");
+        };
+    }
+
+    private void createRoleIfNotExists(IRoleRepository repository, String roleName) {
+        Optional<Role> existingRole = repository.findByName(roleName);
+        log.info("Checking if role {} exists", roleName);
+
+        if (existingRole.isEmpty()) {
+            Role role = Role
+                    .builder()
+                    .name(roleName)
+                    .createdBy("system")
+                    .build();
+
+            repository.save(role);
+            log.info("Role {} created", roleName);
+        }
+    }
 }
