6021d4d39123644ddc7124f2ae357a35532a0835
quikkkk <kexitttttt@gmail.com>
2025-09-13 20:11:47 +0300

    User service

---
+++ user-service/src/test/java/com/dev/quikkkk/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.quikkkk.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
