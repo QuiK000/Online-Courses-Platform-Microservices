99ee26fbe0c37b2fa2870f8b1fccda614076780c
quikkkk <kexitttttt@gmail.com>
2025-09-13 17:54:45 +0300

    implement core domain entities with auditing and security

---
+++ auth-service/src/main/java/com/dev/quikkkk/auth_service/entity/Role.java
@@ -0,0 +1,26 @@
+package com.dev.quikkkk.auth_service.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class Role extends BaseEntity {
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+}
