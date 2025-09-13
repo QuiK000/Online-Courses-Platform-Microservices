93a6305f85619d768f34474bf6bb43d7215d12e1
quikkkk <kexitttttt@gmail.com>
2025-09-13 17:53:33 +0300

    complete authentication module with controller, service and error handling

---
+++ auth-service/src/main/java/com/dev/quikkkk/auth_service/controller/AuthenticationController.java
@@ -0,0 +1,38 @@
+package com.dev.quikkkk.auth_service.controller;
+
+import com.dev.quikkkk.auth_service.dto.request.LoginRequest;
+import com.dev.quikkkk.auth_service.dto.request.RefreshTokenRequest;
+import com.dev.quikkkk.auth_service.dto.request.RegistrationRequest;
+import com.dev.quikkkk.auth_service.dto.response.ApiResponse;
+import com.dev.quikkkk.auth_service.dto.response.AuthenticationResponse;
+import com.dev.quikkkk.auth_service.service.IAuthenticationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final IAuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse<Void>> register(@Valid @RequestBody RegistrationRequest request) {
+        service.register(request);
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<AuthenticationResponse>> login(@Valid @RequestBody LoginRequest request) {
+        return ResponseEntity.ok(ApiResponse.success(service.login(request)));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<ApiResponse<AuthenticationResponse>> refreshToken(@RequestBody RefreshTokenRequest request) {
+        return ResponseEntity.ok(ApiResponse.success(service.refreshToken(request)));
+    }
+}
---
+++ auth-service/src/main/java/com/dev/quikkkk/auth_service/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,120 @@
+package com.dev.quikkkk.auth_service.service.impl;
+
+import com.dev.quikkkk.auth_service.dto.request.LoginRequest;
+import com.dev.quikkkk.auth_service.dto.request.RefreshTokenRequest;
+import com.dev.quikkkk.auth_service.dto.request.RegistrationRequest;
+import com.dev.quikkkk.auth_service.dto.response.AuthenticationResponse;
+import com.dev.quikkkk.auth_service.entity.Role;
+import com.dev.quikkkk.auth_service.entity.User;
+import com.dev.quikkkk.auth_service.exception.BusinessException;
+import com.dev.quikkkk.auth_service.exception.ErrorCode;
+import com.dev.quikkkk.auth_service.mapper.UserMapper;
+import com.dev.quikkkk.auth_service.repository.IRoleRepository;
+import com.dev.quikkkk.auth_service.repository.IUserRepository;
+import com.dev.quikkkk.auth_service.service.IAuthenticationService;
+import com.dev.quikkkk.auth_service.service.IJwtService;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.dev.quikkkk.auth_service.exception.ErrorCode.EMAIL_ALREADY_EXISTS;
+import static com.dev.quikkkk.auth_service.exception.ErrorCode.PASSWORD_MISMATCH;
+import static com.dev.quikkkk.auth_service.exception.ErrorCode.USERNAME_ALREADY_EXISTS;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AuthenticationServiceImpl implements IAuthenticationService {
+    private final static String TOKEN_TYPE = "Bearer";
+
+    private final AuthenticationManager authenticationManager;
+    private final IJwtService jwtService;
+    private final IUserRepository userRepository;
+    private final IRoleRepository roleRepository;
+    private final UserMapper mapper;
+
+    @Override
+    public AuthenticationResponse login(LoginRequest request) {
+        log.info("Login request: {}", request);
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+
+        User user = userRepository
+                .findByUsernameIgnoreCase(request.getUsername())
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+
+        String token = jwtService.generateAccessToken(user);
+        String refreshToken = jwtService.generateRefreshToken(user);
+
+        log.info("Login response: {}", token);
+        return AuthenticationResponse
+                .builder()
+                .accessToken(token)
+                .refreshToken(refreshToken)
+                .tokenType(TOKEN_TYPE)
+                .build();
+    }
+
+    @Override
+    public AuthenticationResponse refreshToken(RefreshTokenRequest request) {
+        log.info("Refresh token request: {}", request);
+        String newAccessToken = jwtService.refreshAccessToken(request.getRefreshToken());
+        return AuthenticationResponse
+                .builder()
+                .accessToken(newAccessToken)
+                .refreshToken(request.getRefreshToken())
+                .tokenType(TOKEN_TYPE)
+                .build();
+    }
+
+    @Override
+    @Transactional
+    public void register(RegistrationRequest request) {
+        log.info("Registration request: {}", request);
+        checkUsername(request.getUsername());
+        checkUserEmail(request.getEmail());
+        checkPasswords(request.getPassword(), request.getConfirmPassword());
+
+        Role defaultRole = roleRepository
+                .findByName("ROLE_STUDENT")
+                .orElseThrow(() -> new EntityNotFoundException("Role user does not exist"));
+
+        Set<Role> roles = new HashSet<>();
+        roles.add(defaultRole);
+
+        User user = mapper.toUser(request);
+        user.setRoles(roles);
+
+        log.debug("Saving user: {}", user);
+        userRepository.save(user);
+        log.info("User {} registered", user.getUsername());
+
+        defaultRole.getUsers().add(user);
+        roleRepository.save(defaultRole);
+    }
+
+    private void checkUserEmail(String email) {
+        boolean emailExists = userRepository.existsByEmailIgnoreCase(email);
+        if (emailExists) throw new BusinessException(EMAIL_ALREADY_EXISTS);
+    }
+
+    private void checkUsername(String username) {
+        boolean usernameExists = userRepository.existsByUsernameIgnoreCase(username);
+        if (usernameExists) throw new BusinessException(USERNAME_ALREADY_EXISTS);
+    }
+
+    private void checkPasswords(String password, String confirmPassword) {
+        if (password == null || !password.equals(confirmPassword)) throw new BusinessException(PASSWORD_MISMATCH);
+    }
+}
