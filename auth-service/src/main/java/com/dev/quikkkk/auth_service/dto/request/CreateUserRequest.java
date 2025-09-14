40d38126ee9bdf1f259423abce9e7e3307a189c6
quikkkk <kexitttttt@gmail.com>
2025-09-14 14:05:12 +0300

    Add `BeansConfig` for shared bean configurations and `CreateUserRequest` DTOs in `user-service` and `auth-service`

---
+++ auth-service/src/main/java/com/dev/quikkkk/auth_service/dto/request/CreateUserRequest.java
@@ -0,0 +1,21 @@
+package com.dev.quikkkk.auth_service.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreateUserRequest {
+    private String id;
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String role;
+}
