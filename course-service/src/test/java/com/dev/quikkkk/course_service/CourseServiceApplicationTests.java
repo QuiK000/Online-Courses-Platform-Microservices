20703a12b3fed881d19ee0f5ec40b4cb22d4dc63
quikkkk <kexitttttt@gmail.com>
2025-09-15 19:42:07 +0300

    Initialize `course-service` with Spring Boot. Add Maven wrapper, application configuration, and essential dependencies.

---
+++ course-service/src/test/java/com/dev/quikkkk/course_service/CourseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.quikkkk.course_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
