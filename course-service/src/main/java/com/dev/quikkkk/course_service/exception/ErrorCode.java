4928781a841b2ab1ed51f764686cfa77508a5f79
quikkkk <kexitttttt@gmail.com>
2025-09-16 18:52:30 +0300

    Add `COURSE_NOT_FOUND` error code to handle missing course scenarios in `course-service` exceptions.

--- course-service/src/main/java/com/dev/quikkkk/course_service/exception/ErrorCode.java
+++ course-service/src/main/java/com/dev/quikkkk/course_service/exception/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
     BAD_REQUEST("BAD_REQUEST", "Bad request", HttpStatus.BAD_REQUEST),
     FORBIDDEN("FORBIDDEN", "Forbidden", HttpStatus.FORBIDDEN),
     UNAUTHORIZED("UNAUTHORIZED", "Unauthorized", HttpStatus.UNAUTHORIZED),
-    INVALID_JWT_TOKEN("INVALID_JWT_TOKEN", "Invalid JWT token", HttpStatus.UNAUTHORIZED);
+    INVALID_JWT_TOKEN("INVALID_JWT_TOKEN", "Invalid JWT token", HttpStatus.UNAUTHORIZED),
+    COURSE_NOT_FOUND("COURSE_NOT_FOUND", "Course not found with id %s" , HttpStatus.NOT_FOUND ),;
 
     private final String code;
     private final String defaultMessage;
