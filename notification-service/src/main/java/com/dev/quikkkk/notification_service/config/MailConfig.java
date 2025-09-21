d7b78e6bbf9ed710a17a494357a4e7107cff6196
quikkkk <kexitttttt@gmail.com>
2025-09-21 13:14:10 +0300

    Update MailConfig to configure JavaMailSender and refine SMTP properties
    - Set up JavaMailSender with host, port, credentials, and additional SMTP properties.
    - Adjusted notification-service.yml to align with updated MailConfig settings.

--- notification-service/src/main/java/com/dev/quikkkk/notification_service/config/MailConfig.java
+++ notification-service/src/main/java/com/dev/quikkkk/notification_service/config/MailConfig.java
@@ -5,10 +5,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import java.util.Properties;
+
 @Configuration
 public class MailConfig {
     @Bean
     public JavaMailSender javaMailSender() {
-        return new JavaMailSenderImpl();
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost("localhost");
+        mailSender.setPort(1025);
+        mailSender.setUsername("root");
+        mailSender.setPassword("root");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.connectiontimeout", "5000");
+        props.put("mail.smtp.timeout", "30000");
+        props.put("mail.smtp.writetimeout", "5000");
+        props.put("mail.smtp.ssl.trust", "*");
+
+        return mailSender;
     }
 }
