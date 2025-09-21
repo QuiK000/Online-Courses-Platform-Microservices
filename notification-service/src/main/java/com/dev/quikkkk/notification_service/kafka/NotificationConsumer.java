6c0c9ffedb95233cd5ec5911ead367e13fca6c05
quikkkk <kexitttttt@gmail.com>
2025-09-21 11:04:44 +0300

    Enhance Kafka configuration and email verification notification handling
    - Added Kafka producer configuration to `auth-service.yml`.
    - Updated `NotificationConsumer` to include `notificationDate` and improved error handling in email verification notifications.
    - Set Kafka `groupId` for `NotificationConsumer`.

--- notification-service/src/main/java/com/dev/quikkkk/notification_service/kafka/NotificationConsumer.java
+++ notification-service/src/main/java/com/dev/quikkkk/notification_service/kafka/NotificationConsumer.java
@@ -10,6 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 import static com.dev.quikkkk.notification_service.document.NotificationType.SEND_EMAIL_VERIFICATION;
 
 @Service
@@ -19,22 +21,29 @@ public class NotificationConsumer {
     private final INotificationRepository repository;
     private final IEmailService emailService;
 
-    @KafkaListener(topics = "code-topic")
+    @KafkaListener(topics = "code-topic", groupId = "notification-service")
     public void consumeCodeEmailVerification(CodeConfirmation notification) throws MessagingException {
         log.info("Received email verification notification: {}", notification);
+        try {
+            repository.save(
+                    Notification.builder()
+                            .type(SEND_EMAIL_VERIFICATION)
+                            .email(notification.email())
+                            .code(notification.code())
+                            .userId(notification.userId())
+                            .notificationDate(LocalDateTime.now())
+                            .build()
+            );
 
-        repository.save(
-                Notification.builder()
-                        .type(SEND_EMAIL_VERIFICATION)
-                        .email(notification.email())
-                        .code(notification.code())
-                        .userId(notification.userId())
-                        .build()
-        );
+            emailService.sendCodeSuccessEmail(
+                    notification.email(),
+                    notification.code()
+            );
 
-        emailService.sendCodeSuccessEmail(
-                notification.email(),
-                notification.code()
-        );
+            log.info("Email verification notification sent successfully to: {}", notification.email());
+        } catch (Exception e) {
+            log.error("Failed to send email verification notification to: {}", notification.email(), e);
+            throw e;
+        }
     }
 }
