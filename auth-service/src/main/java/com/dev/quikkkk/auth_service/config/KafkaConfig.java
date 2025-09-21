55c6e8cd728f2071e5e734c99e04f466ac2a029c
quikkkk <kexitttttt@gmail.com>
2025-09-21 10:59:31 +0300

    Update Kafka bootstrap server address in KafkaConfig

--- auth-service/src/main/java/com/dev/quikkkk/auth_service/config/KafkaConfig.java
+++ auth-service/src/main/java/com/dev/quikkkk/auth_service/config/KafkaConfig.java
@@ -18,7 +18,7 @@ public class KafkaConfig {
     public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> config = new HashMap<>();
 
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.0.101:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.0.100:9092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         config.put(ProducerConfig.ACKS_CONFIG, "all");
