07439e1bcb63ed165be3eddba03ab8f548c0ffca
quikkkk <kexitttttt@gmail.com>
2025-09-26 19:17:30 +0300

    Enhance JWT handling and exception management in `progress-service`
    - Added `KeyUtils` utility to load public keys for validation.
    - Implemented `ErrorCode` enum for consistent error responses.
    - Introduced `BusinessException` to handle application-level exceptions.
    - Enhanced `JwtServiceImpl` to validate and extract claims from tokens securely using `KeyUtils`.
    - Created `ApplicationExceptionHandler` for centralized exception handling.
    - Added `JwtFilter` to handle token-based authentication.
    - Updated `pom.xml` with JJWT dependencies for JWT processing.
    - Introduced `ErrorResponse` DTO for structured error messages.

--- progress-service/src/main/java/com/dev/quikkkk/progress_service/service/impl/JwtServiceImpl.java
+++ progress-service/src/main/java/com/dev/quikkkk/progress_service/service/impl/JwtServiceImpl.java
@@ -1,27 +1,70 @@
 package com.dev.quikkkk.progress_service.service.impl;
 
+import com.dev.quikkkk.progress_service.exception.BusinessException;
 import com.dev.quikkkk.progress_service.service.IJwtService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import utils.KeyUtils;
 
+import java.security.PublicKey;
+import java.util.Date;
 import java.util.List;
 
+import static com.dev.quikkkk.progress_service.exception.ErrorCode.INVALID_JWT_TOKEN;
+
+@Service
+@RequiredArgsConstructor
 public class JwtServiceImpl implements IJwtService {
+    private static final String USER_ID = "userId";
+    private static final String PATH_TO_PUBLIC_KEY = "keys/local-only/public_key.pem";
+
+    private final PublicKey publicKey;
+
+    public JwtServiceImpl() throws Exception {
+        this.publicKey = KeyUtils.loadPublicKey(PATH_TO_PUBLIC_KEY);
+    }
+
     @Override
     public String extractUsername(String token) {
-        return "";
+        return extractClaims(token).getSubject();
     }
 
     @Override
     public String extractUserId(String token) {
-        return "";
+        return extractClaims(token).get(USER_ID).toString();
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public List<String> extractRoles(String token) {
-        return List.of();
+        return (List<String>) extractClaims(token).get("roles");
     }
 
     @Override
     public boolean isTokenValid(String token, String expectedUsername) {
-        return false;
+        String username = extractUsername(token);
+        return username.equals(expectedUsername) && !isTokenExpired(token);
+    }
+
+    private Claims extractClaims(String token) {
+        try {
+            return Jwts
+                    .parser()
+                    .verifyWith(publicKey)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+        } catch (JwtException e) {
+            throw new BusinessException(INVALID_JWT_TOKEN);
+        }
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaims(token)
+                .getExpiration()
+                .before(new Date());
     }
 }
