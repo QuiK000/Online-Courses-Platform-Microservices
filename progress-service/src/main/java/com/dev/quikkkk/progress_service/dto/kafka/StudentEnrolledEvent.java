a2cfd626f314f11079b6f5768211c89796367580
quikkkk <kexitttttt@gmail.com>
2025-09-27 18:18:01 +0300

    Add MongoConfig, Kafka event DTOs, and update Kafka configurations
    - Introduced `MongoConfig` with `@EnableMongoAuditing` for MongoDB auditing support.
    - Added Kafka event DTOs: `StudentEnrolledEvent` and `LessonInfo` for event handling.
    - Updated Kafka configurations in relevant services with a new bootstrap server address (`172.19.94.160`).

---
+++ progress-service/src/main/java/com/dev/quikkkk/progress_service/dto/kafka/StudentEnrolledEvent.java
@@ -0,0 +1,24 @@
+package com.dev.quikkkk.progress_service.dto.kafka;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class StudentEnrolledEvent {
+    private String studentId;
+    private String courseId;
+    private String teacherId;
+    private String courseName;
+    private List<LessonInfo> lessons;
+    private LocalDateTime enrolledAt;
+}
