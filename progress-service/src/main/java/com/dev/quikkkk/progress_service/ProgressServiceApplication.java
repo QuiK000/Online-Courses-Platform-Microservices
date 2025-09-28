76754c1e72648cac1b2b25c6c57662635afa3ec3
quikkkk <kexitttttt@gmail.com>
2025-09-28 14:16:33 +0300

    Enable Feign client support in `progress-service`

--- progress-service/src/main/java/com/dev/quikkkk/progress_service/ProgressServiceApplication.java
+++ progress-service/src/main/java/com/dev/quikkkk/progress_service/ProgressServiceApplication.java
@@ -2,8 +2,10 @@ package com.dev.quikkkk.progress_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ProgressServiceApplication {
 
 	public static void main(String[] args) {
