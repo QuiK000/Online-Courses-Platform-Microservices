41711a9f806c3e27697fbc58a3f35d6813868576
quikkkk <kexitttttt@gmail.com>
2025-09-28 14:46:57 +0300

    Add new notification types to `NotificationType` enum for various events
    - Added `PROGRESS_MILESTONE`, `LESSON_COMPLETED`, `WEEKLY_PROGRESS_REPORT`, `COURSE_COMPLETED`, and `LESSON_REMINDER` enumeration values.

--- notification-service/src/main/java/com/dev/quikkkk/notification_service/document/NotificationType.java
+++ notification-service/src/main/java/com/dev/quikkkk/notification_service/document/NotificationType.java
@@ -1,5 +1,10 @@
 package com.dev.quikkkk.notification_service.document;
 
 public enum NotificationType {
-    SEND_EMAIL_VERIFICATION
+    SEND_EMAIL_VERIFICATION,
+    PROGRESS_MILESTONE,
+    LESSON_COMPLETED,
+    WEEKLY_PROGRESS_REPORT,
+    COURSE_COMPLETED,
+    LESSON_REMINDER
 }
